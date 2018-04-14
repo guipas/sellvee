86787ddd257f2ef65ec6e8ad73866c1de227c16b
Guillaume P <9uillaume@gmail.com>
2018-04-14 17:24:36 +0200

    removed favicon

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -27,8 +26,6 @@ module.exports = (userConfig = {}) => {
   }
   app.set('view engine', 'ejs');
 
-  // uncomment after placing your favicon in /public
-  // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
